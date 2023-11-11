2cbff1c8575bd821ef83529a5ef8ed5a68dd6edb
muhammadfahmi28 <79831020+muhammadfahmi28@users.noreply.github.com>
2023-11-11 23:26:26 +0700

    host fontawsome

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -15,7 +15,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&family=Urbanist:ital,wght@0,200;0,400;0,600;0,700;0,900;1,200;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/efae5deb11.js" crossorigin="anonymous"></script>
+    {{-- <script src="https://kit.fontawesome.com/efae5deb11.js" crossorigin="anonymous"></script> --}}
+    <link rel="stylesheet" href="{{asset('assets/vendor/fontawseome/css/all.min.css')}}">
 
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
     @yield('head')
