20d2b0e54b937219afaf496c7ab31d2d38e88370
muhammadfahmi28 <fahmi.personal96@gmail.com>
2026-03-27 21:55:29 +0700

    fix contain

--- resources/views/pages/gallery/components/video_viewer_base.blade.php
+++ resources/views/pages/gallery/components/video_viewer_base.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="flex-grow-1 flex-shrink-1 h-0 position-relative" style="height: 0%;">
         <div id="video-viewer-main" class="h-100 p-4 text-center">
-            <video controls src="#" type="video/mp4" style="height: 100%;">
+            <video controls src="#" type="video/mp4" style="max-height: 100%; width: 100%;">
                 Your browser does not support the video tag.
             </video>
         </div>
